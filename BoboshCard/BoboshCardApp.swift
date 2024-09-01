1bf8673cd0eb03850fa55bc7cc08358534345ff8
Gozde <gozde.a.997@gmail.com>
2024-09-01 18:16:50 +0300

    Initial Commit

---
+++ BoboshCard/BoboshCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoboshCardApp.swift
+//  BoboshCard
+//
+//  Created by Gözde Aydin on 1.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct BoboshCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
